0513d55e3e8ebffcc54dbf8f41ed166b9e5eaced
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-07-22 16:01:39 -0300

    feat: add prisma to server

--- src/server.js
+++ src/server.js
@@ -3,11 +3,21 @@ const http = require("http");
 const { port } = require("./config/port");
 const { headers } = require("./config/headers");
 const { defineHeaders } = require("./helpers/define-headers");
+
+const { prisma } = require("./lib/prisma-client");
 const { router } = require("./router");
 
 const server = http.createServer((req, res) => {
 	defineHeaders(res, headers);
-	router(req, res);
+	router(req, res)
+		.then(async () => {
+			prisma.$disconnect();
+		})
+		.catch(async (err) => {
+			console.error(err);
+			await prisma.$disconnect();
+			process.exit(1);
+		});
 });
 
 server.listen(port, () => {
