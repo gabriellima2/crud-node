4270e77377106ae6ee445d402e54fddc5fef808b
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-07-22 16:02:23 -0300

    chore: update allow-methods header

--- src/config/headers.js
+++ src/config/headers.js
@@ -1,7 +1,9 @@
+const { HTTP_METHODS: { OPTIONS, GET, POST, DELETE, PATCH } } = require("../helpers/http-methods");
+
 const headers = {
   "Content-Type": "application/json",
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "OPTIONS, GET, POST, DELETE, PUT",
+  "Access-Control-Allow-Methods": `${OPTIONS}, ${GET}, ${POST}, ${DELETE}, ${PATCH}`,
   "Access-Control-Max-Age": "2592000", // 30 Days in Seconds
 }
 
---
+++ src/helpers/http-methods.js
@@ -0,0 +1,13 @@
+const HTTP_METHODS = {
+	GET: "GET",
+	POST: "POST",
+	DELETE: "DELETE",
+	PUT: "POST",
+	PATCH: "PATCH",
+	OPTIONS: "OPTIONS",
+	HEAD: "HEAD"
+}
+
+module.exports = {
+	HTTP_METHODS,
+};
