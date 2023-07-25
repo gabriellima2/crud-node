42935d053c1beb2daf8c0cf3b3f116dd531fca43
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-07-25 17:23:02 -0300

    feat: add the create method to task-repository

--- src/repositories/task-repository.js
+++ src/repositories/task-repository.js
@@ -1,6 +1,9 @@
+const { prisma } = require("../lib/prisma-client");
+
 class TaskRepository {
-	async create(params) {
-		return params;
+	async create(data) {
+		const createdTask = await prisma.task.create({ data });
+		return Object.freeze(createdTask);
 	};
 }
 
